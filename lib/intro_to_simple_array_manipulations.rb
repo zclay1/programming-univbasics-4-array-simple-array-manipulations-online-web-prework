bd3ea63daf1260f35b7752f6e3a837264e179ce6
Zhari Clay <zhariclay@gmail.com>
2019-09-21 00:47:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, 2)
-  array.delete_at(2)
+def using_delete_at(array, num)
+  array.delete_at(num)
 end
 
 
